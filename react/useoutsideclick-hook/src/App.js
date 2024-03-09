c3b5bd26ccf5c5f67a436fd83e8693cc74da9dfe
prerana1821 <prerananawar1@gmail.com>
2024-03-10 02:13:59 +0530

    add: custom useoutsideclick hook

---
+++ react/useoutsideclick-hook/src/App.js
@@ -0,0 +1,45 @@
+import * as React from "react";
+import toast, { Toaster } from "react-hot-toast";
+import "./App.css";
+
+const useOutsideClick = (ref, callback) => {
+  React.useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  });
+
+  const handleOutsideClick = (event) => {
+    // won't work for all the cases
+    // if (ref.current.innerHTML !== event.target.innerHTML) {
+    //   callback();
+    // }
+
+    // right solution
+    if (!ref.current || !ref.current.contains(event.target)) {
+      callback();
+    }
+  };
+};
+
+export default function App() {
+  const centeredRef = React.useRef(null);
+
+  useOutsideClick(centeredRef, () => {
+    console.log("Clicked Outside");
+    toast("Clicked Outside 🚀❤️‍🔥");
+  });
+
+  return (
+    <div>
+      <Toaster reverseOrder={true} />
+      <div className='container'>
+        <div className='centered-box' ref={centeredRef}>
+          <p>Click Outside To Show Toast</p>
+        </div>
+      </div>
+    </div>
+  );
+}
