3cc360ceb7ae4fcdf1f5c676ead083926fa22235
prerana1821 <prerananawar1@gmail.com>
2024-03-17 04:07:23 +0530

    add: useKeyPress Custom Hook

---
+++ react/usekeypress-hook/src/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import useKeyPress from "./useKeyPress";
+import "./App.css";
+
+export default function App() {
+  const shiftKey = useKeyPress("Shift");
+  const enterKey = useKeyPress("Enter");
+
+  useEffect(() => {
+    if (shiftKey && enterKey) {
+      showToast();
+    }
+  }, [shiftKey, enterKey]);
+
+  const showToast = () => {
+    toast.success(`Hello world!`);
+  };
+
+  return (
+    <div>
+      <Toaster position='top-center' reverseOrder={false} />
+      <div className='container'>
+        <h1>Shift + Enter</h1>
+
+        <p>Hit Shift + Enter to show a toast! 🎉</p>
+      </div>
+    </div>
+  );
+}
