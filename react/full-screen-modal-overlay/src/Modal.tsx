b7011275b61744b0adf8ee2e603bb136ac59c3a2
prerana1821 <prerananawar1@gmail.com>
2024-04-12 05:13:31 +0530

    add: modal change

--- react/full-screen-modal-overlay/src/Modal.tsx
+++ react/full-screen-modal-overlay/src/Modal.tsx
@@ -13,15 +13,56 @@ export default function Modal({
           event.stopPropagation();
         }}
       >
-        <h1>Hello from Modal</h1>
-        <button
-          className='button'
-          onClick={() => {
-            setShowModal(false);
-          }}
-        >
-          Close Modal
-        </button>
+        <div className='modal-header'>
+          <h1>Hello from Modal</h1>
+        </div>
+        <div className='modal-body'>
+          <div className='modal-content'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+            numquam ducimus adipisci quae? Minima aliquid vel autem tempore
+            consequuntur accusantium odio quod nam eius, atque, earum laboriosam
+            saepe vero dolores quaerat quidem, sit nostrum natus perferendis
+            delectus. Suscipit, nulla maiores. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Nulla voluptatibus, libero recusandae
+            accusamus possimus, illum vero vitae repudiandae temporibus illo
+            porro! Sunt odit natus ad itaque magni, temporibus quisquam maxime
+            libero aut aliquam nobis minus voluptate numquam, totam eligendi
+            voluptatem harum quas. Ut, suscipit temporibus! Cum commodi deserunt
+            nulla ab pariatur earum aspernatur sit architecto repellat voluptas
+            debitis, quos tempore consectetur deleniti. Unde assumenda dolore
+            minus enim praesentium error deserunt repellendus nam exercitationem
+            aut, ipsam eveniet tempora facere placeat fugit odio voluptate
+            reiciendis ratione vero eligendi. Magnam quia, necessitatibus
+            reiciendis eveniet vero, quaerat porro obcaecati maiores mollitia,
+            harum quos architecto. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Autem numquam ducimus adipisci quae? Minima
+            aliquid vel autem tempore consequuntur accusantium odio quod nam
+            eius, atque, earum laboriosam saepe vero dolores quaerat quidem, sit
+            nostrum natus perferendis delectus. Suscipit, nulla maiores. Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Nulla
+            voluptatibus, libero recusandae accusamus possimus, illum vero vitae
+            repudiandae temporibus illo porro! Sunt odit natus ad itaque magni,
+            temporibus quisquam maxime libero aut aliquam nobis minus voluptate
+            numquam, totam eligendi voluptatem harum quas. Ut, suscipit
+            temporibus! Cum commodi deserunt nulla ab pariatur earum aspernatur
+            sit architecto repellat voluptas debitis, quos tempore consectetur
+            deleniti. Unde assumenda dolore minus enim praesentium error
+            deserunt repellendus nam exercitationem aut, ipsam eveniet tempora
+            facere placeat fugit odio voluptate reiciendis ratione vero
+            eligendi. Magnam quia, necessitatibus reiciendis eveniet vero,
+            quaerat porro obcaecati maiores mollitia, harum quos architecto.
+          </div>
+        </div>
+        <div className='modal-footer'>
+          <button
+            className='button'
+            onClick={() => {
+              setShowModal(false);
+            }}
+          >
+            Close Modal
+          </button>
+        </div>
       </div>
     </div>
   );
